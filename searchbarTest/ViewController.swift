4becb647982b1897f8886c44e755f48067f5abb1
Dario Fanjul <dario@MacBook-Pro-de-Dario-5.local>
2014-10-30 22:35:29 -0300

    Initial Commit

---
+++ searchbarTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  searchbarTest
+//
+//  Created by Dario Fanjul on 30/10/14.
+//  Copyright (c) 2014 Dario Fanjul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
